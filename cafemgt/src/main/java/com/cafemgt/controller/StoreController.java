90168307961b86266029d8121730927b51b7d565
y95e <79968010+y95e@users.noreply.github.com>
2021-03-15 21:35:38 +0900

    [2team] - 최영은 수정한파일명- CommonController, StoreController, owner, store 수정내용 - 컨트롤러, 폴더 생성 및 화면 이동경로 설정 날짜 - 2021/03/15

--- cafemgt/src/main/java/com/cafemgt/controller/StoreController.java
+++ cafemgt/src/main/java/com/cafemgt/controller/StoreController.java
@@ -3,6 +3,7 @@ package com.cafemgt.controller;
 import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StoreController {
@@ -12,6 +13,42 @@ public class StoreController {
 		System.out.println("======================================");
 		System.out.println("======= StoreController bean 등록 ======");
 		System.out.println("======================================");
+	}
+	
+	@GetMapping("/addstore")
+	public String addstore() {
+		
+		return "store/addstore";		
+	}
+	
+	@GetMapping("/addcustomer")
+	public String addcustomer() {
+		
+		return "store/addcustomer";	
+	}
+	
+	@GetMapping("/adduser")
+	public String adduser() {
+		
+		return "store/adduser";	
+	}
+	
+	@GetMapping("/getstore")
+	public String getstore() {
+		
+		return "store/getstore";	
+	}
+	
+	@GetMapping("/getcustomer")
+	public String getcustomer() {
+		
+		return "store/getcustomer";	
+	}
+	
+	@GetMapping("/getuser")
+	public String getuser() {
+		
+		return "store/getuser";	
 	}
 
 }
