10dd9211f92c899456f04e661734c4c422059caf
wujujin <62084934+wujujin@users.noreply.github.com>
2021-03-16 09:52:34 +0900

    [2team]-2team 기초세팅 2021-03-16

---
+++ cafemgt/src/main/java/com/cafemgt/dao/ArticleMapper.java
@@ -0,0 +1,8 @@
+package com.cafemgt.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ArticleMapper {
+
+}
---
+++ cafemgt/src/main/java/com/cafemgt/dto/OwnerDto.java
@@ -0,0 +1,5 @@
+package com.cafemgt.dto;
+
+public class OwnerDto {
+
+}
